d1bc8d11057c9ba210f5d4b161fc1ece66eeef26
jeiza <jeyjww@gmail.com>
2019-08-14 16:37:11 -0300

    first commit

---
+++ prova/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          animal peixe =  new animal();
+            peixe.Idade = 2;
+            Console.WriteLine(peixe.Idade);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ prova/animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova
+{
+    class animal
+    {
+        private int idade;
+        bool vivo = false;
+        public int Idade
+        {
+            get
+            {
+                if (vivo)
+                    return idade;
+                else
+                    return 0;
+            }
+            set
+            {
+                if (value > 0)
+                    idade = value;
+            }
+            
+        }
+
+
+
+    }
+}
